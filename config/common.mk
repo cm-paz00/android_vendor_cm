46141ca292eaa779630274ebeeec4510cce7a6b8
Misha Komarovskiy <zombah@gmail.com>
2015-11-13 00:18:31 +0300

    cm: Update extraction of Android Terminal
    libjackpal-androidterm4.so is now libjackpal-androidterm5.so
    
    Signed-off-by: Misha Komarovskiy <zombah@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -102,7 +102,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES +=  \
     vendor/cm/proprietary/Term.apk:system/app/Term.apk \
-    vendor/cm/proprietary/lib/armeabi/libjackpal-androidterm4.so:system/lib/libjackpal-androidterm4.so
+    vendor/cm/proprietary/lib/armeabi/libjackpal-androidterm5.so:system/lib/libjackpal-androidterm5.so
 
 # Bring in camera effects
 PRODUCT_COPY_FILES +=  \
